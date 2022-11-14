d956ae526c3451608fd40c66672021280063af06
Anny Bevilacqua <anny.bevilacqua@studio.unibo.it>
2022-11-14 21:33:39 +0100

    Add colors

--- E001/colors.h
+++ E001/colors.h
@@ -5,7 +5,19 @@
 
 namespace color {
 
+	const glm::vec4 white = glm::vec4(1.0);
+	const glm::vec4 black = glm::vec4(0.0, 0.0, 0.0, 1.0);
+	const glm::vec4 transparent = glm::vec4(0.0);
+
+	const glm::vec4 purple = glm::vec4(1.0, 0.0, 1.0, 1.0);
+	const glm::vec4 red = glm::vec4(1.0, 0.0, 0.0, 1.0);
+	const glm::vec4 yellow = glm::vec4(1.0, 1.0, 0.0, 1.0);
+	const glm::vec4 green = glm::vec4(0.0, 1.0, 0.0, 1.0);
 	const glm::vec4 cyan = glm::vec4(0.0, 1.0, 1.0, 1.0);
+	const glm::vec4 blue = glm::vec4(0.0, 0.0, 1.0, 1.0);
+
+	const glm::vec4 dark_yellow = vec4(255.0 / 255.0, 75.0 / 255.0, 0.0, 1.0);
+	const glm::vec4 darkOrange = vec4(150.0 / 255.0, 75.0 / 255.0, 0.0, 1.0);
 
 }
 
